3f94e26dd0da05d3436102e3db61f90ef3651e3c
Shivam Kumar <shivam.saha06@gmail.com>
2021-10-03 15:18:09 +0530

    create tribonacci_series.cpp

---
+++ DYNAMIC PROGRAMMING/QUESTION 1/tribonacci_series.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+void tribonacci_series(int n)
+{
+    int dp[n];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+ 
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    return dp[n];
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int ans = tribonacci_series(n);
+    cout<<ans;
+    return 0;
+}
