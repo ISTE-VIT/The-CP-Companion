eb2411c2ed179b69b6e881e796a34a62c2c1b38a
Harshit <62823486+harshit7962@users.noreply.github.com>
2021-10-05 10:54:03 +0530

    improvised lazyNumbers solution in c++
    This code access less memory space and is much concise.
    The auxiliary space here is O(n) and time complexity is also O(n)

--- RECURSION/QUESTION 2/sol_qn2.cpp
+++ RECURSION/QUESTION 2/sol_qn2.cpp
@@ -1,28 +1,22 @@
 #include<iostream>
+
 using namespace std;
 
-void lazy(int n,int c,int d)
-{
-    if(c==2*d)
-    {
-        cout<<n<<' ';
+void lazyNumbers(int n) {
+    if(n==0) {
+        cout << n << " ";
         return;
     }
-    if(n>0 && c<d)
-    {
-        cout<<n<<' ';
-        lazy(n=n-1,c=c+1,d);
-    }else{
-        cout<<n<<' ';
-        lazy(n=n+1,c=c+1,d);
-    }
-    return;
+    cout << n << " ";
+    lazyNumbers(n-1);
+    cout << n << " ";
 }
 
 int main()
 {
     int n;
-    cin>>n;
-    lazy(n,0,n);
+    cin >> n;
+    lazyNumbers(n);
+
     return 0;
 }
