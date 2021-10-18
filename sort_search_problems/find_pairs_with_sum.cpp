adece6dfe471097c2ff02e1844c2b0ad14dac772
N-E-W-T-O-N <38786893+N-E-W-T-O-N@users.noreply.github.com>
2021-10-18 11:17:26 +0530

    search and sort

---
+++ sort_search_problems/find_pairs_with_sum.cpp
@@ -0,0 +1,48 @@
+/*
+ * Problem: 
+ * Given an unsorted array of integers, find a pair with given sum in it.
+ * For example,
+ * Input:
+ * arr = [8, 7, 2, 5, 3, 1]
+ * sum = 10
+ *
+ * Output: 
+ * Pair found at index 0 and 2 (8 + 2)
+ * OR
+ * Pair found at index 1 and 4 (7 + 3)
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+void printPairWithSum(const std::vector<int>& arr, const int sum)
+{
+    std::unordered_map<int, int> hashMap;
+    bool found = false;
+    for (unsigned int i = 0; i < arr.size(); ++i)
+    {
+        // We need to check if pair(arr[i], sum-arr[i]) already
+        // exists in the map
+        //
+        if (hashMap.find(sum-arr[i]) != hashMap.end())
+        {
+            std::cout << "Found pair with sum: " << sum
+                << " with values (" << sum-arr[i] << " ," << arr[i]
+                << ") at indices (" <<  hashMap[sum-arr[i]] << " ,"
+                << i << ")" << std::endl;
+            found = true;
+        }
+        hashMap[arr[i]] = i;
+    }
+    if (!found) std::cout << "Pair not found\n";
+}
+
+int main()
+{
+    std::vector<int> arr {8, 7, 2, 5, 3, 1};
+    printPairWithSum(arr, 10);
+    return 0;
+}
